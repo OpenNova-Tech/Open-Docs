3deab6424d6c5f02c7d4aae859b310499e512543
Tijo <bhattacharjeeanurag3@gmail.com>
2025-06-08 22:18:16 +0530

    some pages

---
+++ app/docs/page.tsx
@@ -0,0 +1,23 @@
+import { GradientCard1 } from '@/components/ui/gradient-card-1'
+import { GradientCard2 } from '@/components/ui/gradient-card-2'
+import { GradientCard3 } from '@/components/ui/gradient-card-3'
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div className="grid md:grid-cols-3 p-4">
+        <Link href='/language'>
+          <GradientCard1 />
+        </Link>
+          <GradientCard2 />
+        <Link href='/backend'>
+          <GradientCard3 />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default page
