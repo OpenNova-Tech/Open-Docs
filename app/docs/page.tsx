c6447da83dd61f740554d2590a2cb91c96105243
Tijo <hamsen3880@outlook.com>
2025-07-11 13:19:53 +0530

    profile and docs page changes

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,122 +1,122 @@
-import React from "react";
-import { Card } from '@/components/Card';
+import React from 'react'
+import { Card } from '@/components/Card'
 
 const sections = [
   {
-    title: "Languages, Libraries, Frameworks and Software Tools",
+    title: 'Languages, Libraries, Frameworks and Software Tools',
     cards: [
       {
-        title: "Programming Languages",
-        subtitle: "Code logic using powerful languages",
-        image: "/docs/languages.jpg",
-        readMoreLink: "/language"
+        title: 'Programming Languages',
+        subtitle: 'Code logic using powerful languages',
+        image: '/docs/languages.jpg',
+        readMoreLink: '/language'
       },
       {
-        title: "Website and Application Development",
-        subtitle: "Build modern tech applications",
-        image: "/docs/development.webp",
-        readMoreLink: "/development"
+        title: 'Website and Application Development',
+        subtitle: 'Build modern tech applications',
+        image: '/docs/development.webp',
+        readMoreLink: '/development'
       },
       {
-        title: "Software Tools",
-        subtitle: "Tools enhancing productivity and automation",
-        image: "/docs/tools.jpg",
+        title: 'Software Tools',
+        subtitle: 'Tools enhancing productivity and automation',
+        image: '/docs/tools.jpg',
       },
     ],
   },
   {
-    title: "Technology Domains and Sectors",
+    title: 'Technology Domains and Sectors',
     cards: [
       {
-        title: "Cloud Computing",
-        subtitle: "Deploy scalable services on cloud",
-        image: "/docs/cloud.webp",
+        title: 'Cloud Computing',
+        subtitle: 'Deploy scalable services on cloud',
+        image: '/docs/cloud.webp',
       },
       {
-        title: "Machine Learning",
-        subtitle: "Train models with meaningful data",
-        image: "/docs/aiml.webp",
-        readMoreLink: "/aiml"
+        title: 'Machine Learning',
+        subtitle: 'Train models with meaningful data',
+        image: '/docs/aiml.webp',
+        readMoreLink: '/aiml'
       },
       {
-        title: "Dev-Ops",
-        subtitle: "Streamline development and deployment pipelines",
-        image: "/docs/devops.webp",
+        title: 'Dev-Ops',
+        subtitle: 'Streamline development and deployment pipelines',
+        image: '/docs/devops.webp',
       },
       {
-        title: "Cybersecurity",
-        subtitle: "Protect systems from digital threats",
-        image: "/docs/cyber.webp",
+        title: 'Cybersecurity',
+        subtitle: 'Protect systems from digital threats',
+        image: '/docs/cyber.webp',
       },
     ],
   },
   {
-    title: "Other Technologies",
+    title: 'Other Technologies',
     cards: [
       {
-        title: "Web 3.0",
-        subtitle: "Decentralized future of the web",
-        image: "/docs/web3.jpg",
+        title: 'Web 3.0',
+        subtitle: 'Decentralized future of the web',
+        image: '/docs/web3.jpg',
       },
       {
-        title: "Internet of Things",
-        subtitle: "Smart devices communicating through internet",
-        image: "/docs/iot.jpg",
-        readMoreLink: "/iot"
+        title: 'Internet of Things',
+        subtitle: 'Smart devices communicating through internet',
+        image: '/docs/iot.jpg',
+        readMoreLink: '/iot'
       },
     ],
   },
   {
-    title: "System Knowledge and Theory",
+    title: 'System Knowledge and Theory',
     cards: [
       {
-        title: "Operating Systems",
-        subtitle: "Foundation managing hardware and software",
-        image: "/docs/os.jpg",
+        title: 'Operating Systems',
+        subtitle: 'Foundation managing hardware and software',
+        image: '/docs/os.jpg',
       },
       {
-        title: "System Design",
-        subtitle: "Design scalable and maintainable architecture",
-        image: "/docs/system.png",
+        title: 'System Design',
+        subtitle: 'Design scalable and maintainable architecture',
+        image: '/docs/system.png',
       },
       {
-        title: "Theoretical Computer Science",
-        subtitle: "Mathematical foundations behind computation",
-        image: "/docs/theory.jpg",
+        title: 'Theoretical Computer Science',
+        subtitle: 'Mathematical foundations behind computation',
+        image: '/docs/theory.jpg',
       },
       {
-        title: "Data Structures & Algorithms",
-        subtitle: "Master logic, speed, and code.",
-        image: "/docs/cp.webp",
-        readMoreLink: "/cp"
+        title: 'Data Structures & Algorithms',
+        subtitle: 'Master logic, speed, and code.',
+        image: '/docs/cp.webp',
+        readMoreLink: '/cp'
       },
     ],
   },
-];
+]
 
 
 
 const page = () => {
   return (
-  <div className="pt-32 px-20 bg-black pb-16">
-    {sections.map((section, sectionIndex) => (
-      <div key={sectionIndex}>
-        <p className="text-neutral-500 py-10">{section.title}</p>
-        <div className="grid grid-cols-1 sm:grid-cols-4 gap-4">
-          {section.cards.map((card, cardIndex) => (
-            <Card
-              key={cardIndex}
-              title={card.title}
-              subtitle={card.subtitle}
-              image={card.image}
-              readMoreLink={card.readMoreLink}
-            />
-          ))}
+    <div className='pt-32 px-20 bg-black pb-16'>
+      {sections.map((section, sectionIndex) => (
+        <div key={sectionIndex}>
+          <p className='text-neutral-500 py-10'>{section.title}</p>
+          <div className='grid grid-cols-1 sm:grid-cols-4 gap-4'>
+            {section.cards.map((card, cardIndex) => (
+              <Card
+                key={cardIndex}
+                title={card.title}
+                subtitle={card.subtitle}
+                image={card.image}
+                readMoreLink={card.readMoreLink}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
-);
+      ))}
+    </div>
+  )
 
 }
 
--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,13 +1,12 @@
 'use client'
 
 import ProfileCard from '@/components/Profile'
-// import { Github, Instagram, Linkedin, Twitter } from 'lucide-react'
 import { IconBrandGithub, IconBrandInstagram, IconBrandLinkedin, IconBrandX } from '@tabler/icons-react'
 
 export default function Page() {
   return (
     <div className='bg-black'>
-<div className='max-w-3xl mx-auto pt-40 text-center space-y-4'>
+      <div className='max-w-3xl mx-auto pt-40 text-center space-y-4'>
         <h2 className='text-3xl font-bold text-indigo-400'>Meet the Team Behind OpenDocs</h2>
         <p className='text-gray-300 text-lg'>
           The OpenDocs project is powered by a small but passionate team of developers, writers, and open-source
@@ -17,34 +16,34 @@ export default function Page() {
           From frontend frameworks to backend stacks — if it&apos;s worth learning, it&apos;s worth documenting right.
         </p>
       </div>
-    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-8 bg-black p-10 '>
-      <ProfileCard
-        name='Anurag Bhattacharjee'
-        username='Founder'
-        message='Yea I made this. To thank me follow on Tinder.'
-        image='/private/pichar-removebg.png'
-        gradientFrom='black'
-        gradientTo='#6633ff'
-        socials={[
-          {
-            icon: <IconBrandGithub className='text-white w-6 h-6' />,
-            url: 'https://github.com/DarkmodeWorking'
-          },
-          {
-            icon: <IconBrandInstagram className='text-pink-400 w-6 h-6' />,
-            url: 'https://www.instagram.com/_.hamsen._/'
-          },
-          {
-            icon: <IconBrandLinkedin className='text-blue-400 w-6 h-6' />,
-            url: 'https://www.linkedin.com/in/anurag-bhattacharjee-65a487275/'
-          },
-          {
-            icon: <IconBrandX className='text-teal-400 w-6 h-6' />,
-            url: 'https://x.com/hammydoestweet8'
-          }
-        ]}
-      />
-    </div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 gap-8 bg-black p-10 '>
+        <ProfileCard
+          name='Anurag Bhattacharjee'
+          username='Founder'
+          message='Yea I made this. To thank me follow on Tinder.'
+          image='/private/pichar-removebg.png'
+          gradientFrom='black'
+          gradientTo='#6633ff'
+          socials={[
+            {
+              icon: <IconBrandGithub className='text-white w-6 h-6' />,
+              url: 'https://github.com/DarkmodeWorking'
+            },
+            {
+              icon: <IconBrandInstagram className='text-pink-400 w-6 h-6' />,
+              url: 'https://www.instagram.com/_.hamsen._/'
+            },
+            {
+              icon: <IconBrandLinkedin className='text-blue-400 w-6 h-6' />,
+              url: 'https://www.linkedin.com/in/anurag-bhattacharjee-65a487275/'
+            },
+            {
+              icon: <IconBrandX className='text-teal-400 w-6 h-6' />,
+              url: 'https://x.com/hammydoestweet8'
+            }
+          ]}
+        />
+      </div>
     </div>
   )
 }
