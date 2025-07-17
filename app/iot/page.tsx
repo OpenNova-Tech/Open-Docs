0442d275bb50f5956b362320b713a96221c48bee
Anurag Bhattacharjee <hamsen3880@outlook.com>
2025-07-17 19:49:22 +0530

    Update page.tsx

--- app/iot/page.tsx
+++ app/iot/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+'use client'
 
-import React from "react";
-import { HoverEffect } from "@/components/ui/card-hover-effect";
+import React from "react"
+import { HoverEffect } from "@/components/ui/card-hover-effect"
 
 export default function LanguagePage() {
 	const systems = [
