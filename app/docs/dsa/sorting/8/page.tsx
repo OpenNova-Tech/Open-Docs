d5727984e73fe99255cf3d206d7cd8eb9f3ceec3
Tijo <hamsen3880@outlook.com>
2025-10-10 18:03:50 +0530

    fixed merge sort sorting/8

--- app/docs/dsa/sorting/8/page.tsx
+++ app/docs/dsa/sorting/8/page.tsx
@@ -74,18 +74,15 @@ merge_sort(arr)
 print(arr)`
 
 	const code2j = `class MergeSort {
-    void merge(int arr[], int l, int m, int r) {
+    static void merge(int arr[], int l, int m, int r) {
         int n1 = m - l + 1;
         int n2 = r - m;
-
         int L[] = new int[n1];
         int R[] = new int[n2];
-
         for (int i = 0; i < n1; ++i)
             L[i] = arr[l + i];
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
-
         int i = 0, j = 0;
         int k = l;
         while (i < n1 && j < n2) {
@@ -98,13 +95,11 @@ print(arr)`
             }
             k++;
         }
-
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
-
         while (j < n2) {
             arr[k] = R[j];
             j++;
@@ -112,7 +107,7 @@ print(arr)`
         }
     }
 
-    void sort(int arr[], int l, int r) {
+    static void sort(int arr[], int l, int r) {
         if (l < r) {
             int m = (l + r) / 2;
             sort(arr, l, m);
@@ -123,7 +118,8 @@ print(arr)`
 
     public static void main(String args[]) {
         int arr[] = {5, 3, 8, 4, 2};
-        new MergeSort().sort(arr, 0, arr.length - 1);
+        sort(arr, 0, arr.length - 1);
+        System.out.print("Sorted array: ");
         for (int num : arr)
             System.out.print(num + " ");
     }
