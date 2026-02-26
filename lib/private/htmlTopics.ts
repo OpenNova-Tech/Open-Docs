932e52c3eefcdcebd9053d8a9fcbdba46f67d8e5
Tijo <hamsen3880@outlook.com>
2026-02-27 00:57:48 +0530

    html addition

---
+++ lib/private/htmlTopics.ts
@@ -0,0 +1,16 @@
+export const HTML_TOPICS = [
+  'introduction',
+  'history',
+  'setup-environment',
+  'hello-world',
+  'comments',
+  'compilation-process',
+  'input-and-output',
+  'data-types-and-variables',
+  'constants-and-literals',
+  'type-casting',
+  'operators',
+  'control-flow',
+  'loops',
+  'exception-handling'
+]
